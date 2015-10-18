bf14ca38b1aa4509eb7665eb2baf2f132725b40a
cdmiller540 <cdmiller540@ufl.edu>
2015-10-18 11:47:53 -0400

    Created rough storyboard

---
+++ memoryBook/changeInfoController.swift
@@ -0,0 +1,13 @@
+//
+//  changeInfoController.swift
+//  memoryBook
+//
+//  Created by Cody Miller on 10/18/15.
+//  Copyright © 2015 Cody Miller. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class changeInfoController: UIViewController{
+    
+}
---
+++ memoryBook/changePasswordController.swift
@@ -0,0 +1,13 @@
+//
+//  changePasswordController.swift
+//  memoryBook
+//
+//  Created by Cody Miller on 10/18/15.
+//  Copyright © 2015 Cody Miller. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class changePasswordController: UIViewController{
+    
+}
