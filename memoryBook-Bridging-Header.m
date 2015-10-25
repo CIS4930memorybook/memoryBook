00ba033fa265ecf6a0cd8d8d0e9202402c7d03a3
cdmiller540 <cdmiller540@ufl.edu>
2015-10-25 16:13:42 -0400

    Added backend support from Parse

---
+++ memoryBook-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  memoryBook-Bridging-Header.m
+//  memoryBook
+//
+//  Created by Cody Miller on 10/25/15.
+//  Copyright © 2015 Cody Miller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
